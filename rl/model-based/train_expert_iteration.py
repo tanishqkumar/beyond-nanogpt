6cd6248270f21a90c8be0caec6a7572f284b8762
tanishqkumar <thetanishq@gmail.com>
2025-05-23 03:11:41 -0400

    expert iteration almost there

--- rl/model-based/train_expert_iteration.py
+++ rl/model-based/train_expert_iteration.py
@@ -53,17 +53,16 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 # Set device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# buffer stores/feeds batches for policy, ie. (s, a) pairs so [sdim, adim]
-class Buffer: 
-    def __init__(self, buff_sz=10_000, sdim=3, adim=1): 
-        self.buff = torch.zeros(buff_sz, sdim + adim + sdim, device=device)
+# buffer stores/feeds batches for policy, ie. (s, a_opt) pairs so [sdim, adim]
+class PolicyBuffer: 
+    def __init__(self, buff_sz=10_000, sdim=8, adim=4): 
+        self.buff = torch.zeros(buff_sz, sdim + adim, device=device)
         self.ptr = 0 
         self.sdim = sdim 
         self.adim = adim 
         self.size = 0
         self.max_size = buff_sz
 
-    
     def push(self, batch): # gets [sdim, adim sdim]
         # batch = torch.tensor(batch) #[b, sdim + adim + sdim]
         b, _ = batch.shape 
@@ -81,19 +80,63 @@ class Buffer:
         self.size = min(self.size + b, self.max_size)
         return 
 
-    def get_batch(self, bsz): 
-        if self.size < bsz:
-            bsz = self.size
+    def get_batch(self, batch_size): 
+        if self.size < batch_size:
+            batch_size = self.size
             
-        indices = torch.randint(0, self.size, (bsz,), device=device)
+        indices = torch.randint(0, self.size, (batch_size,), device=device)
         batch = self.buff[indices]
         
         # split the batch into state, action, and delta state
-        s = batch[:, :self.sdim]
-        a = batch[:, self.sdim:self.sdim+self.adim]
-        ds = batch[:, self.sdim+self.adim:]
+        s_batch = batch[:, :self.sdim]
+        a_batch = batch[:, self.sdim:]
         
-        return s, a, ds
+        return s_batch, a_batch
+
+
+# stores [s, val] pairs from mcts rollouts to train valueNet on, so [sdim, 1]
+class ValueBuffer: 
+    def __init__(self, buff_sz=10_000, sdim=8): 
+        self.buff = torch.zeros(buff_sz, sdim + 1, device=device)
+        self.ptr = 0 
+        self.sdim = sdim 
+        self.size = 0
+        self.max_size = buff_sz
+
+    def push(self, batch): # gets [sdim, 1]
+        b, _ = batch.shape 
+
+        if self.ptr + b > self.buff.shape[0]: 
+            # loop around to beginning and overwrite 
+            overflow = self.ptr + b - self.buff.shape[0]
+            self.buff[self.ptr:] = batch[:b-overflow]
+            self.buff[:overflow] = batch[b-overflow:]
+            self.ptr = overflow 
+        else: # usual case
+            self.buff[self.ptr:self.ptr+b] = batch 
+            self.ptr = (self.ptr + b) % self.max_size
+
+        self.size = min(self.size + b, self.max_size)
+        return 
+
+    def get_batch(self, batch_size): 
+        if self.size < batch_size:
+            batch_size = self.size
+            
+        indices = torch.randint(0, self.size, (batch_size,), device=device)
+        batch = self.buff[indices]
+        
+        # split the batch into state, val 
+        s = batch[:, :self.sdim]
+        v = batch[:, -1]
+        
+        return s, v
+
+
+# in hindsight, I should probably use one buffer class 
+    # with diff .policy_buff and .val_buff and get_value_batch and get_policy_batch 
+    # functions, but nbd
+
 
 class Node: 
     def __init__(self, state, nactions=4, action_taken=None, prior=None, parent=None):
@@ -104,6 +147,8 @@ class Node:
         self.count = 0
 
         self.children = {} # action -> Node 
+
+        # not aesthetically optimal that prior is tensor and untried_actions a list but nbd
         self.prior = prior if prior is not None else torch.ones(nactions) / nactions # action -> prob from policy(state)
         self.untried_actions = list(range(nactions)) # all actions to start 
     
@@ -136,11 +181,14 @@ def backprop(node, final_val):
 def action_distb(s, policy): 
     return F.softmax(policy(s), dim=-1)
 
+
+# each MCTS sim in num_sums can be broken down into 
+    # [Selection, Expansion, Simulation, Backprop] and I've annotated in the code
 def MCTS(
     env: gym.Env, 
-    state: torch.Tensor, 
-    policy_net: nn.Module, # gives priors for exploration
-    value_net: nn.Module, # used to compute values of nodes that we write in 
+    state: torch.Tensor, # starting state s0 from env to use as root.state = s0
+    policy_net: nn.Module, # gives priors for exploration for use in action_distb
+    value_net: nn.Module, # used to compute values of nodes at terminal state for +V(s_term)
     num_sims: int = 100, 
     max_depth: int = 20, # depth of exploration starting from a leaf 
     cpuct: float = 1.0, # exploration constant
@@ -156,7 +204,6 @@ def MCTS(
         prior=action_distb(state, policy_net), 
         parent=None, 
     )
-
     
     for _ in range(num_sims): 
         env_copy = deepcopy(env) # clone this original env so we don't pollute 
@@ -252,7 +299,7 @@ class ValueNet:
 @dataclass 
 class TrainConfig: 
     lr: float = 1e-4 
-    bsz: int = 128 
+    batch_size: int = 128 
     nsteps: int = 1_000
     num_rollouts: int = 100
     buff_sz: int = 10_000
@@ -261,28 +308,46 @@ class TrainConfig:
     eval_policy_every: int = 100 
     sdim: int = 8 
     adim: int = 4
+    env_name: str = 'LunarLander-v2'
+    cpuct: float = 1.0 
+    discount: float = 0.99 
     
+
 def train(
-    env: gym.Env, 
     cfg: TrainConfig = TrainConfig()
 ): 
-    env = gym.make('LunarLander-v2')
+    env = gym.make(cfg.env_name)
     policy_net = PolicyNet(); value_net = ValueNet()
     policy_opt = torch.optim.AdamW(policy_net.parameters(), lr=cfg.lr)
-    buffer = Buffer()
+    value_opt = torch.optim.AdamW(value_net.parameters(), lr=cfg.lr)
+    policy_buffer = PolicyBuffer()
+    value_buffer = ValueBuffer()
+
+
+    MCTS_ARGS = (
+        env.clone(), 
+        s, 
+        policy_net, 
+        value_net, 
+        cfg.num_sums, 
+        cfg.max_depth, 
+        cfg.cpuct, 
+        cfg.discount
+    )
 
     for step in range(cfg.nsteps): 
         for _ in range(cfg.num_rollouts): 
             done = False 
             s, _ = env.reset() 
             while not done: 
-                a_next = MCTS(env.clone(), s, value_net)
-                buffer.push((s, a_next))
+                a_next = MCTS(*MCTS_ARGS)
+                singleton_batch = torch.tensor([[s, a_next]]) # [1, sdim adim]
+                policy_buffer.push(singleton_batch) 
                 _, _, done, _, _ = env.step(a_next)
 
         for _ in range(cfg.num_policy_updates_per_step): 
-            state_batch, opt_action_batch = buffer.get_batch(cfg.bsz)
-            loss = F.CrossEntropLoss(policy_net(state_batch), opt_action_batch)
+            state_batch, opt_action_batch = policy_buffer.get_batch(cfg.batch_size)
+            loss = F.cross_entropy(policy_net(state_batch), opt_action_batch)
             loss.backward()
             policy_opt.step()
             policy_opt.zero_grad()
@@ -295,3 +360,4 @@ def train(
             print(f'Policy gets reward {eval(policy_net)}')
 
         
+# TODO: add argparse to take in args and launch train, etc 
